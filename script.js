ddd5ad6aefb9840dbd0cd17f2881c517ec236b09
jb3152 <jb3152@student.ohs.school.nz>
2025-05-31 18:50:41 +1200

    explainer

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-// Team letter hover functionality
+// Get all the team letter elements (A, B, C)
 const letters = document.querySelectorAll(".teamAt,.teamBt,.teamCt");
+// Get the element where the description will be displayed
 const displayText = document.getElementById("displayText");
 
-// Get the description elements
+// Get the hidden description elements for each team
 const desOneBox = document.getElementById("descriptions-one");
 const descriptOne = desOneBox ? desOneBox.innerText : '';
 const desTwoBox = document.getElementById("descriptions-two");
@@ -10,6 +11,7 @@ const descriptTwo = desTwoBox ? desTwoBox.innerText : '';
 const desThreeBox = document.getElementById("descriptions-three");
 const descriptThree = desThreeBox ? desThreeBox.innerText : '';
 
+// When you hover over a letter, show the correct description and highlight the letter
 letters.forEach(letter => {
     letter.addEventListener("mouseover", () => {
         let text = '';
@@ -22,33 +24,28 @@ letters.forEach(letter => {
     });
 });
 
-// Team button click functionality
+// Get references to each team letter by ID
 const teamA = document.getElementById("teamA");
 const teamB = document.getElementById("teamB");
 const teamC = document.getElementById("teamC");
 
-const selected = document.querySelectorAll(".selected");
-
-//textbox to change when button pressed
-const descript = document.getElementById("displayText");
-var currTeam = 0;
-
+// Function to update the description and highlight when a letter is clicked
 function printText(num, team){
     switch (num) {
         case 1:
-            descript.textContent = descriptOne;
+            displayText.textContent = descriptOne;
             teamA.classList.add("selected");
             teamB.classList.remove("selected");
             teamC.classList.remove("selected");
             break;
         case 2:
-            descript.textContent = descriptTwo;
+            displayText.textContent = descriptTwo;
             teamA.classList.remove("selected");
             teamB.classList.add("selected");
             teamC.classList.remove("selected");
             break;
         case 3:
-            descript.textContent = descriptThree;
+            displayText.textContent = descriptThree;
             teamA.classList.remove("selected");
             teamB.classList.remove("selected");
             teamC.classList.add("selected");
@@ -58,17 +55,13 @@ function printText(num, team){
     }
 }
 
+// When you click a letter, show the correct description and highlight it
 teamA.onclick = function(){
-    currTeam = 1;
-    printText(currTeam, teamA);
+    printText(1, teamA);
 };
-
 teamB.onclick = function(){
-    currTeam = 2;
-    printText(currTeam, teamB);
+    printText(2, teamB);
 };
-
 teamC.onclick = function(){
-    currTeam = 3;
-    printText(currTeam, teamC);
+    printText(3, teamC);
 };
