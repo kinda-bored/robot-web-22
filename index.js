1f2b00658636dacf9c76c04c94b59b5da64ab5d1
jb3152 <jb3152@student.ohs.school.nz>
2025-06-10 11:55:35 +1200

    remove cookie

--- index.js
+++
@@ -1,8 +0,0 @@
-
-if (localStorage.getItem("cookieSeen") != "shown") {
-  $(".cookie-banner").delay(2000).fadeIn();
-  localStorage.setItem("cookieSeen","shown")
-};
-$(".close").click(function() {
-  (".cookie-banner").fadeOut();
-})
